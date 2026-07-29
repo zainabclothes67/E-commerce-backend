70fae19a0865fd4167e920b950a06d245e324499
zainabclothes67 <zainabclothes67@gmail.com>
2026-07-30 00:46:41 +0500

    Adeel feat fix the issue

--- src/utils/generateOrderId.js
+++ src/utils/generateOrderId.js
@@ -1,18 +1,12 @@
-// import { OrderModel } from "../models/OrderModel";
+const { OrderModel } = require("../models/OrderModel");
 
-// export const generateOrderId = async (totalOrders:any): Promise<string> => {
-//   for (let attempt = 0; attempt < 10; attempt++) {
-//     const num = Math.floor(10000 + Math.random() * 90000);
-//     const orderId = `MSO-${num}`;
-//     const exists = await OrderModel.exists({ orderId });
-//     if (!exists) return orderId;
-//   }
-//   throw new Error("Failed to generate a unique order ID");
-// };
-
-
-const generateOrderId = (totalOrdersDB) => {
-  // console.log("Order Number", totalOrdersDB)
-  return `MSO-${1000 + totalOrdersDB}`;
+const generateOrderId = async () => {
+  for (let attempt = 0; attempt < 10; attempt++) {
+    const num = Math.floor(10000 + Math.random() * 90000);
+    const orderId = `MSO-${num}`;
+    const exists = await OrderModel.exists({ orderId });
+    if (!exists) return orderId;
+  }
+  throw new Error("Failed to generate a unique order ID");
 };
 module.exports.generateOrderId = generateOrderId;
